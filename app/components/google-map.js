f9695820de328bf166b026592b910564e4d1a765
xorrr <derxor@gmail.com>
2015-07-21 00:25:20 +0200

    display sample map in index route

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  insertMap: Ember.on('didInsertElement', function() {
+    var options = {
+      center: new window.google.maps.LatLng(
+        this.get('latitude'),
+        this.get('longitude')
+      ),
+      zoom: 15
+    };
+    new window.google.maps.Map(this.$('.map-canvas')[0], options);
+  })
 });
