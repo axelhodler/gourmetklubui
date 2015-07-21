9f7f18263b481848f30afde129ad5c6c454d7d6b
xorrr <derxor@gmail.com>
2015-07-21 23:49:49 +0200

    refactor google maps component

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -3,21 +3,22 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   insertMap: Ember.on('didInsertElement', function() {
-    var centerLatLng = new window.google.maps.LatLng(this.get('latitude'),this.get('longitude'));
+    var gmaps = window.google.maps;
+    var centerLatLng = new gmaps.LatLng(this.get('latitude'),this.get('longitude'));
     var options = {
       center: centerLatLng,
       zoom: 13,
-      mapTypeId: window.google.maps.MapTypeId.HYBRID
+      mapTypeId: gmaps.MapTypeId.HYBRID
     };
-    var map = new window.google.maps.Map(this.$('.map-canvas')[0], options);
+    var map = new gmaps.Map(this.$('.map-canvas')[0], options);
 
-    var geocoder = new window.google.maps.Geocoder();
+    var geocoder = new gmaps.Geocoder();
 
     this.get('restaurants').forEach(function(restaurant) {
       geocoder.geocode( { 'address': restaurant.get('address')}, function(results, status) {
-        if (status === window.google.maps.GeocoderStatus.OK) {
-          if (status !== window.google.maps.GeocoderStatus.ZERO_RESULTS) {
-            new window.google.maps.Marker({
+        if (status === gmaps.GeocoderStatus.OK) {
+          if (status !== gmaps.GeocoderStatus.ZERO_RESULTS) {
+            new gmaps.Marker({
               position: results[0].geometry.location,
               map: map,
               title: restaurant.get('name')
