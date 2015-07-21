43e101cc748625a019eb0a982c4b6bb503f2c923
xorrr <derxor@gmail.com>
2015-07-21 22:17:32 +0200

    display sample marker in the map

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -3,14 +3,17 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   insertMap: Ember.on('didInsertElement', function() {
+    var centerLatLng = new window.google.maps.LatLng(this.get('latitude'),this.get('longitude'));
     var options = {
-      center: new window.google.maps.LatLng(
-        this.get('latitude'),
-        this.get('longitude')
-      ),
+      center: centerLatLng,
       zoom: 13,
       mapTypeId: window.google.maps.MapTypeId.HYBRID
     };
-    new window.google.maps.Map(this.$('.map-canvas')[0], options);
+    var map = new window.google.maps.Map(this.$('.map-canvas')[0], options);
+    new window.google.maps.Marker({
+      position: centerLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
   })
 });
